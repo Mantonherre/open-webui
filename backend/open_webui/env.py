569dba481b422a46f347c9ca6ae59ece301dc37f
M.antonherre <m.antonherre@gmail.com>
2026-01-16 11:47:19 +0100

    Personalización onehilo: branding, icons, config y constantes

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -87,9 +87,9 @@ if "cuda_error" in locals():
 
 SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "GVA IA")
+if WEBUI_NAME != "GVA IA":
+    WEBUI_NAME += " (GVA IA)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
